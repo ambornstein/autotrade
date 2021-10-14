c65d66126181a6ec58c67183975fc9c9c1320bdd
Andrei Bornstein <amborn02@gmail.com>
2021-10-14 14:30:14 -0400

    fixing

--- autotrade.py
+++ autotrade.py
@@ -26,7 +26,6 @@ def get_data(short, range, tick):
     data_macd = get_macd(data['Close'], 26, 12, 9)
     data_macd['Color'] = np.where(data_macd['hist']<0, 'red', 'green')
     data_macd.tail()
-    data.merge(data_macd, on='Date', how='inner')
     return data
 
 btc_data = get_data("1d", "3mo", "BTC-USD")
